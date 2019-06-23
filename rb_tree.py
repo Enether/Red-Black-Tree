008ed96e669e5ba44d9686a0a34449499926e6f7
Gabriel Filipe <gabrielfilipe7unb@gmail.com>
2019-06-23 15:25:34 -0400

    Adds the docstring from the original source

--- rb_tree.py
+++ rb_tree.py
@@ -610,6 +610,42 @@ class RedBlackTree:
 
     def _build_tree_string(self, root=None, curr_index=0,
                            index=False, delimiter='-'):
+
+        """
+
+        Recursively walk down the binary tree and build a pretty-print string.
+        In each recursive call, a "box" of characters visually representing the
+        current (sub)tree is constructed line by line. Each line is padded with
+        whitespaces to ensure all lines in the box have the same length.
+
+        Then the box, its width, and start-end positions of its root node value
+        repr string (required for drawing branches) are sent up to the parent
+        call.
+
+        The parent call then combines its left and right sub-boxes
+        to build a larger box etc.
+
+        :param root: Root node of the binary tree.
+        :type root: binarytree.Node | None
+        :param curr_index: Level-order_ index of the current node
+                           (root node is 0).
+        :type curr_index: int
+        :param index: If set to True, include the level-order_ node indexes
+                      using the following format:
+                     ``{index}{delimiter}{value}`` (default: False).
+        :type index: bool
+        :param delimiter: Delimiter character between the node index
+                          and the node value (default: '-').
+        :type delimiter:
+        :return: Box of characters visually representing the current subtree,
+                 width of the box, and start-end positions of the repr string
+                 of the new root node value.
+        :rtype: ([str], int, int, int)
+        .. _Level-order:
+            https://en.wikipedia.org/wiki/Tree_traversal#Breadth-first_search
+
+        """
+
         if curr_index == 0:
             root = self.root
         if root is None:
